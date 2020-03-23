1619756b7d3045b4d097c1109b2f26e6b278b6d7
Roberto <robq97@hotmail.com>
2020-03-22 19:15:45 -0600

    Added layout component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from '../components/navbar/Navbar';
+import Layout from '../components/layout/layout'
 import Login from '.././components/login/Login';
 import Cambio_Contraseña from '../components/forms/sistema/cambioContraseña/Cambio_contraseña';
 import Reiniciar_sesion from '../components/forms/sistema/reiniciarSesion/Reiniciar_sesion';
 
 class App extends Component {
- 
+
   render() {
     return (
-      <div className="App">
-        <Navbar user="nombreUsuario" />
-        <div className="container">
-
+      <div>
+        <Layout>
           {/* testing components */}
 
           {/*<Login /> */}
           {/*<Reiniciar_sesion />*/}
           <Cambio_Contraseña />
 
-        </div>
+        </Layout>
       </div>
     );
   }
