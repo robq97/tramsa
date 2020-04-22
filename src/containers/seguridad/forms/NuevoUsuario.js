80da0eac85f2ab870a8c306e5c24fffb7227d744
Cristhian Martínez <cmartinezh184@ulacit.ed.cr>
2020-04-21 19:22:20 -0600

    Logs loaded to table, changed Usuario form in order to be able to add to DB

--- src/containers/seguridad/forms/NuevoUsuario.js
+++ src/containers/seguridad/forms/NuevoUsuario.js
@@ -20,9 +20,9 @@ class NuevoUsuario extends Component {
             name: '',
             password: '',
             passwordValidation: '',
-            status: '',
+            status: 'Activo',
             roles: [],
-            role: ''
+            rol: ''
         }
     }
 
@@ -41,7 +41,7 @@ class NuevoUsuario extends Component {
     }
 
     validateEqualPasswords = () => {
-        if (this.state.password === this.state.newPasswordValidation) {
+        if (this.state.password === this.state.passwordValidation) {
             return true;
         } else {
             return false
@@ -60,13 +60,14 @@ class NuevoUsuario extends Component {
         }
 
         if (this.validateEqualPasswords()) {
-            axios.post(URL.concat('user/create'), { data })
+            axios.post(URL.concat('user/signup'), { data })
                 .then((res) => {
                     alert(res.data.message);
                     document.getElementById("create-user-form").reset();
                 })
                 .catch(error => console.error('Error: ', error));
         } else {
+            console.log(data);
             alert('Las conrtaseñas no coinciden');
         }
 
@@ -76,22 +77,22 @@ class NuevoUsuario extends Component {
         return (
             <Card>
                 <Title title="Nuevo Usuario" titleType="title-form" />
-                <form id="create-user-form">
+                <form id="create-user-form" onSubmit={this.handleSubmit}>
                     <div className="row align-items-center">
                         <div className="col pl-5">
                             <Input
                                 smallId="" name="email" smallTxt="Correo Electrónico"
-                                icon="email" id="" placeholder="Correo electrónico" type="email" required={true} />
+                                icon="email" id="" placeholder="Correo electrónico" type="email" required={true} onChange={(ev) => this.handleChange(ev.target)} />
                         </div>
                         <div className="col">
                             <Input
                                 smallId="" name="userName" smallTxt="Usuario"
-                                icon="perm_identity" id="" placeholder="Usuario" type="text" required={true} />
+                                icon="perm_identity" id="" placeholder="Usuario" type="text" required={true} onChange={(ev) => this.handleChange(ev.target)} />
                         </div>
                         <div className="col pr-5">
                             <Input
                                 smallId="" name="name" smallTxt="Nombre"
-                                icon="perm_identity" id="" placeholder="Nombre" type="txt" required={true} />
+                                icon="perm_identity" id="" placeholder="Nombre" type="txt" required={true} onChange={(ev) => this.handleChange(ev.target)} />
                         </div>
                     </div>
                     <div className="row align-items-center">
@@ -99,7 +100,8 @@ class NuevoUsuario extends Component {
                             {/*<Select
                                 URL={URL.concat('rol')} name="rol" property="ROL_Nombre"
                             smallId="ROL_Nombre" smallTxt="Seleccione el rol" />*/}
-                            <select name="rol">
+                            <small>Rol</small>
+                            <select name="rol" onChange={(ev) => this.handleChange(ev.target)}>
                                 {this.state.roles.map((roles) => (
                                     <option key={roles.ROL_Codigo}>{roles.ROL_Nombre}</option>
                                 ))}
@@ -109,23 +111,28 @@ class NuevoUsuario extends Component {
                         <div className="col">
                             <Input
                                 smallId="" name="password" smallTxt="Contraseña"
-                                icon="lock" id="" placeholder="Contraseña" type="password" required={true} />
+                                icon="lock" id="" placeholder="Contraseña" type="password" required={true} onChange={(ev) => this.handleChange(ev.target)} />
                         </div>
                         <div className="col pr-5">
                             <Input
                                 smallId="" name="passwordValidation" smallTxt="Confirmar Contraseña"
-                                icon="lock" id="" placeholder="Confirmar contraseña" type="password" required={true} />
+                                icon="lock" id="" placeholder="Confirmar contraseña" type="password" required={true} onChange={(ev) => this.handleChange(ev.target)} />
                         </div>
                     </div>
                     <div className="row align-items-center">
                         <div className="col pl-5">
-                            <Radio
+                            {/*<Radio
                                 smallId="" name="status" smallTxt="Seleccione un estado"
-                                value1="activo" txt1="Activo" value2="inactivo" txt2="Inactivo" />
+                            value1="activo" txt1="Activo" value2="inactivo" txt2="Inactivo" onChange={(ev) => this.handleChange(ev.target)}/>*/}
+                            <small>Estado</small>
+                            <select name="estado" onChange={(ev) => this.handleChange(ev.target)}>
+                                <option name="Activo" value="Activo">Activo</option>
+                                <option name="Inactivo" value="Inactivo">Inactivo</option>
+                            </select>
                         </div>
                     </div>
                     <div className="text-center">
-                        <Button type="" icon="send" btnTxt="Agregar Usuario" />
+                        <Button type="submit" icon="send" btnTxt="Agregar Usuario" />
                     </div>
                 </form>
             </Card>
