c4e4d26d02f7932d8f67e7e0f7c2641ecafa8958
Roberto <robq97@hotmail.com>
2020-04-23 21:48:21 -0600

    All Consultas tables translated

--- src/containers/consultas/tables/Bitacora.js
+++ src/containers/consultas/tables/Bitacora.js
@@ -12,10 +12,10 @@ class Bitacora extends Component {
                     title={<Translate content="tituloConsultaBitacora" />} titleType="title-form" />
                 <Table
                     url="http://apitramsa.azurewebsites.net/bitacora/"
-                    header1="Usuario" accessor1="BIT_Usuario" filterable1={true} width1={200} show1={true}
-                    header2="Fecha/Hora" accessor2="BIT_FCH_Hora" filterable2={true} width2={300} show2={true}
-                    header3="Código de Registro" accessor3="BIT_COD_Registro" filterable3={false} width3={150} show3={true}
-                    header4="Descripción" accessor4="BIT_Descripcion" filterable4={false} width4={450} show4={true}
+                    header1={<Translate content="tableUsuario" />} accessor1="BIT_Usuario" filterable1={true} width1={200} show1={true}
+                    header2={<Translate content="tableBitacora" />} accessor2="BIT_FCH_Hora" filterable2={true} width2={300} show2={true}
+                    header3={<Translate content="tablaCodigoRegistro" />} accessor3="BIT_COD_Registro" filterable3={false} width3={150} show3={true}
+                    header4={<Translate content="tableDescripción" />} accessor4="BIT_Descripcion" filterable4={false} width4={450} show4={true}
                     show5={false}
                     show6={false}
                     inputShow={false}
